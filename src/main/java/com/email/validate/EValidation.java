f25289433d0bd244ef97bc69f49a368b695766ad
ab923837 <abdullahnegahban@ta29d.wpa.dal.ca>
2017-02-27 14:28:04 -0400

    validate method added(initial commit)

---
+++ src/main/java/com/email/validate/EValidation.java
@@ -0,0 +1,40 @@
+package com.email.validate;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+
+public class EValidation 
+{
+	//to keep track of number of rules the email passed
+    private static int num=0;
+    
+    public static void validate(String mail){
+    	//array to store characters
+    	char[] words = new char[mail.length()];
+    	//storing the characters in order in array
+    	for(int i=0; i< mail.length(); i++){
+    		words[i] = mail.charAt(i);
+    	}
+    	//checker for @
+    	for(int k=0; k < mail.length(); k++){
+    		if(words[k] == '@'){
+    			num++;
+    		}
+    	}
+    	//checker for .
+    	for(int k=0; k < mail.length(); k++){
+    		if(words[k] == '.'){
+    			num++;
+    		}
+    	}
+    	System.out.println(num);
+    }
+    
+    //for testing
+    public static void main(String[]args){
+    	String mail = "testin@gmail.com";
+    	validate(mail);
+    	
+    }
+}
