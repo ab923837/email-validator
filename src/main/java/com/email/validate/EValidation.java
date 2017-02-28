41135f20a9674b5461cba5b6ef68cd16f216275f
ab923837 <abdullahnegahban@ta29d.wpa.dal.ca>
2017-02-28 19:14:00 -0400

    added rules of my own

--- src/main/java/com/email/validate/EValidation.java
+++ src/main/java/com/email/validate/EValidation.java
@@ -8,26 +8,49 @@ public class EValidation
 {
 	//to keep track of number of rules the email passed
     private static int num=0;
+
+    
+    public static void main(String[]args){
+    	String test = "123456543201@gmail.com";
+    	System.out.println(validate(test));
+    }
     
     public static int validate(String mail){
     	//array to store characters
     	char[] words = new char[mail.length()];
-    	//storing the characters in order in array
+    	char[] rule = {'@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm'}; 
+    	
+    	//stores mail characters into array
     	for(int i=0; i< mail.length(); i++){
     		words[i] = mail.charAt(i);
     	}
-    	//checker for @
+    	
+    	//RULE: MUST use @gmail.com
+    	if(mail.matches(".*@gmail.com")){
+    		num++;
+    	}
+
+    	/*
+    	 * RULE: # of character must be greater or equal to 6
+    	 * AND less or equal to 16
+    	*/
+    	if(mail.length() >= 16 && mail.length() <= 22){
+    		num++;
+    	}
+    	
+    	
+    	//RULE: checker for @
     	for(int k=0; k < mail.length(); k++){
     		if(words[k] == '@'){
     			num++;
     		}
     	}
-    	//checker for .
+    	//RULE: checker for .
     	for(int k=0; k < mail.length(); k++){
     		if(words[k] == '.'){
     			num++;
     		}
     	}
     	return num;
-    }
+   }
 }
